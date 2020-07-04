f18df3ec4d35ddbfc6d60b5d65e5238206dc8bdd
Austin Chang <austinchang@Austinde-MacBook.local>
2020-07-04 16:56:11 +0800

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Austin Chang on 2020/7/2.
+//  Copyright © 2020 Austin Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
