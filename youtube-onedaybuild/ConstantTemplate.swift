b299985c47105ed082b5fb85eb76721262d6b498
Austin Chang <austinchang@Austinde-MacBook.local>
2020-07-05 11:35:06 +0800

    modifygitignore to stop tracking Constants.swift and add ConstantTemplete.swift instead

--- youtube-onedaybuild/ConstantTemplate.swift
+++ youtube-onedaybuild/ConstantTemplate.swift
@@ -1,16 +1,17 @@
 //
-//  Constants.swift
+//  ConstantTemplate.swift
 //  youtube-onedaybuild
 //
-//  Created by Austin Chang on 2020/7/4.
+//  Created by Austin Chang on 2020/7/5.
 //  Copyright © 2020 Austin Chang. All rights reserved.
 //
 
 import Foundation
-
+/*
 struct Constants {
     static var API_KEY = ""
     static var PLAYLIST_ID = "UUJ5v_MCY6GNUBTO8-D3XoAg"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
 }
+*/
